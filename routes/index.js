16a295b67e3d87a7bbf49de4e40e4d2944f3cc91
Hamid Teimouri <hamidteimouri@outlook.com>
2019-11-09 17:51:02 +0330

    install mongo driver

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect('mongodb://localhost:27017/cln', function (err, client) {
+        if (err) throw err;
+
+        var db = client.db('cln');
+
+        db.collection('mammals').find().toArray(function (err, result) {
+            if (err) throw err;
+
+            console.log(result)
+        });
+    });
+    //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
