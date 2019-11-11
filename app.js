99ac21f134e2e05976de88be7096685f788a37e9
Hamid Teimouri <hamidteimouri@outlook.com>
2019-11-11 15:07:08 +0330

    changed

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api');
-var coinRouter = require('./routes/coins');
 
 
 var app = express();
@@ -26,9 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* Routing */
 app.use('/', indexRouter);
-app.use('/api', apiRouter);
-app.use('/users', usersRouter);
-app.use('/coins', coinRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function (req, res, next) {
-    var MongoClient = require('mongodb').MongoClient;
-
-    MongoClient.connect('mongodb://localhost:27017/live_price', function (err, client) {
-        if (err) throw err;
-
-        var db = client.db('live_price');
-
-        db.collection('coins').find().toArray(function (err, result) {
-            if (err) throw err;
-
-            console.log(result)
-        });
-    });
-    //res.render('index', { title: 'Express' });
+    res.render('home/index');
 });
 
 module.exports = router;
