1b9af7a6c61d99dcc699ddf18f109c363af1fa38
Hamid Teimouri <hamidteimouri@outlook.com>
2019-11-18 15:23:41 +0330

    changed

--- controller/CoinController.js
+++ controller/CoinController.js
@@ -2,18 +2,20 @@ var CoinModel = require('../model/Coin');
 
 
 exports.store = function (req, res, next) {
+
     var coin = new CoinModel(
         {
-            asset: req.asset,
-            name_fa: req.name_fa,
+            //asset: req.asset,
+            //name_fa: req.name_fa,
         }, function (err, result) {
             if (err) return handleError(err);
             // object saved here
             res.send('Object saved successfully');
         });
 
-
 };
+
+
 exports.index = function (req, res, next) {
     CoinModel.find({}, function (err, result) {
         res.render('coin/index', {
--- model/Coin.js
+++ model/Coin.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var CoinSchema = require('../migration/coins.js');
-var CoinModel = mongoose.model('Coin', CoinSchema);
+
 
 // coinSchema.statics =
 
@@ -31,6 +31,6 @@ coinSchema.statics = {
 };
 */
 
-
+var CoinModel = mongoose.model('Coin', CoinSchema);
 module.exports = CoinModel;
 
